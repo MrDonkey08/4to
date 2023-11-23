7dac54b2316a2677c120f3887b8e8fbfcfa071cd
Alan <113253976+MrDonkey08@users.noreply.github.com>
2023-11-23 15:17:12 -0600

    Correcciones de Sintaxis

--- arquitectura-de-computadoras/practicas/7-unidad-de-control/unidad-de-control.vhdl
+++ arquitectura-de-computadoras/practicas/7-unidad-de-control/unidad-de-control.vhdl
@@ -1,37 +1,39 @@
 LIBRARY IEEE;
-USE IEEE. STD LOGIC 1164.ALL;
-USE IEEE. STD LOGIC ARITH.ALL;
-USE IEEE.STD LOGIC UNSIGNED.ALL;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 ENTITY control IS
 	PORT (
-		reset : IN STD LOGIC;
-		clk : IN STD LOGIC;
+		reset : IN STD_LOGIC;
+		clk : IN STD_LOGIC;
 		irq : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-		rw : INOUT STD LOGIC;
-		datain : IN STD LOGIC VECTOR (3 DOWNTO 0);
-		pcontrol : INOUT STD LOGIC VECTOR (7 DOWNTO 0);
-		re : IN STD LOGIC VECTOR (3 DOWNTO 0);
-		pcout : IN STD LOGIC VECTOR (7 DOWNTO 0);
-		cs : INOUT STD LOGIC VECTOR (4 DOWNTO 0));
+		rw : INOUT STD_LOGIC;
+		datain : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+		pcontrol : INOUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+		rc : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+		pcout : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		cs : INOUT STD_LOGIC_VECTOR (4 DOWNTO 0));
 END control;
+
 ARCHITECTURE Behavioral OF control IS
 	TYPE estados IS (d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16, d17);
-	SIGNAL edo presente, edo futuro : estados;
-	SIGNAL f : std logic vector (7 DOWNTO 0);
+	SIGNAL edo_presente, edo_futuro : estados;
+	SIGNAL f : STD_LOGIC_VECTOR (7 DOWNTO 0);
 BEGIN
-	procesoA : PROCESS (edo presente, irq, reset)
+	procesoA : PROCESS (edo_presente, irq, reset)
 	BEGIN
 		IF reset = '1' THEN
-			edo futuro <= d0;
+			edo_futuro <= d0;
 			pcontrol <= "11111111";
 			rw <= '0';
 			cs <= "11110";
 		ELSE
-			CASE edo presente IS
+			CASE edo_presente IS
 				WHEN d0 =>
 					IF irq - "10" THEN
 						cs <= "11100";
-						edo futuro <= d1;
+						edo_futuro <= d1;
 					ELSIF irq = "01" THEN
 						cs <= "11100";
 						edo_futuro <= d1;
@@ -42,161 +44,161 @@ BEGIN
 				WHEN d1 =>
 					IF irq - "10" THEN
 						pcontrol <= "01000000";
-						edo futuro <= dl;
+						edo_futuro <= dl;
 					ELSIF irq = "01" THEN
 						pcontrol <= "10000000";
-						edo futuro <= di;
+						edo_futuro <= di;
 					ELSE
-						ca = "11110";
+						cs = "11110";
 						edo_futuro <= d3;
 					END IF;
 				WHEN d2 =>
-					CE <= CS;
-					edo futuro <= d5;
-				WHEN d3 ->
-					cE = "10110";
+					cs <= cs;
+					edo_futuro <= d5;
+				WHEN d3 =>
+					cs = "10110";
 					rw <= '1';
-					edo futuro <= d4;
-				WHEN d4 ->
-					EN <= '0';
+					edo_futuro <= d4;
+				WHEN d4 =>
+					rw <= '0';
 					IF datain = "0000" THEN
-						cs <- "00001";
+						cs <= "00001";
 						edo_futuro <= d2;
 					ELSIF datain = "0001" THEN
 						cs <= "00010";
 						edo_futuro <= d5;
 					ELSIF datain = "0010" THEN
 						cs <= "00011";
-						edo_futuroc = d5;
+						edo_futuro = d5;
 					ELSIF datain - "0011" THEN
-						c8 <= "00100";
-						edo futuro <= d5;
+						cs <= "00100";
+						edo_futuro <= d5;
 					ELSIF datain - "0100" THEN
-						cs <- "00101";
-						edo futuro <= d5;
+						cs <= "00101";
+						edo_futuro <= d5;
 					ELSIF datain - "0101" THEN
 						cs <= "00110";
-						edo futuro <= d5;
+						edo_futuro <= d5;
 					ELSIF datain = "0110" THEN
 						cs <= "11111";
-						edo_futuro <- d6;
+						edo_futuro <= d6;
 					ELSIF datain = "0111" THEN
 						cs <= "11111";
-						edo futuro <= d7;
+						edo_futuro <= d7;
 					ELSIF datain = "1000" THEN
 						cs <= "11111";
-						edo futuro <= d8;
+						edo_futuro <= d8;
 					ELSIF datain = "1001" THEN
-						cs <- "00111";
-						edo_futuro <- d9;
+						cs <= "00111";
+						edo_futuro <= d9;
 					ELSIF datain = "1010" THEN
-						cs <- "11011";
+						cs <= "11011";
 						edo_futuro <= d0;
 					ELSIF datain = "1011" THEN
 						cs <= "11111";
 						IF rc(0) = '1' THEN
 							edo_futuro <= d10;
 						ELSE
-							edo_futuroč = d0;
+							edo_futuro = d0;
 						END IF;
 					ELSIF datain = "1100" THEN
 						cs <= "11111";
-						IF ro(2) = '1' THEN
-							edo futuro <= d0;
+						IF rc(2) = '1' THEN
+							edo_futuro <= d0;
 						END IF;
 					ELSIF datain = "1101" THEN
-						cs <- "11111";
+						cs <= "11111";
 						IF rc(1) = '1' THEN
 							edo_futuro <= d10;
 						ELSE
 							edo_futuro <= d0;
 						END IF;
 					ELSIF datain = "1110" THEN
-						@s <- "11111";
+						@s <= "11111";
 						IF rc(3) - '1' THEN
 							edo_futuro <= d10;
 						ELSE
-							edo_futuro <- d;
+							edo_futuro <= d0;
 						END IF;
 					END IF;
 				WHEN d5 =>
-					cs <- "10101";
-					edo futuro <= d11;
+					cs <= "10101";
+					edo_futuro <= d11;
 				WHEN d6 =>
-					cs <- "10110";
-					IW = '1';
+					cs <= "10110";
+					rw = '1';
 					edo_futuro <= d12;
 				WHEN d7 =>
 					cs <= "10110";
-					IN <= '1';
+					rw <= '1';
 					edo_futuro <= d11;
 				WHEN dB =>
-					cE <- "10110";
-					IW = 1';
-					edo futuro <= d15;
+					cs <= "10110";
+					rw = 1';
+					edo_futuro <= d15;
 				WHEN d9 =>
-					cs <- "11000";
-					edo futuro <= d10;
+					cs <= "11000";
+					edo_futuro <= d10;
 				WHEN d10 =>
 					cs <= "10110";
 					rw = '1';
-					edo futuro <= d16;
-				WHEN d11 ->
-					ca <- "10001";
-					EW <= '0';
-					edo futuro <= d0;
-				WHEN d12 ->
+					edo_futuro <= d16;
+				WHEN d11 =>
+					cs <= "10001";
+					rw <= '0';
+					edo_futuro <= d0;
+				WHEN d12 =>
 					cs <= "11111";
-					IN <= '0';
-					pcontrol(0) <- datain(0);
-					pcontrol (1) <= datain(1);
+					rw <= '0';
+					pcontrol(0) <= datain(0);
+					pcontrol(1) <= datain(1);
 					pcontrol(2) <= datain(2);
-					pcontrol(3) <- datain(3);
-					pcontrol(4) <- pcontro1(4);
-					pcontrol (5) <- pcontrol(5);
+					pcontrol(3) <= datain(3);
+					pcontrol(4) <= pcontro1(4);
+					pcontrol(5) <= pcontrol(5);
 					pcontrol(6) <= pcontrol(6);
-					pcontrol(7) <= pcontrol (7);
-					edo futuro <= d13;
+					pcontrol(7) <= pcontrol(7);
+					edo_futuro <= d13;
 				WHEN d13 =>
-					Cs <= "10110";
+					cs <= "10110";
 					rw <= '1';
-					edo_futuro <- d14;
+					edo_futuro <= d14;
 				WHEN d14 =>
 					c & <= "11010";
-					Ew <= '0';
-					pcontrol(0) <- pcontrol (0) :
-					pcontrol (1) <= pcontrol (1);
-					pcontrol(2) <= pcontrol (2);
-					pcontrol (3) <= pcontrol (3);
+					rw <= '0';
+					pcontrol(0) <= pcontrol(0) :
+					pcontrol(1) <= pcontrol(1);
+					pcontrol(2) <= pcontrol(2);
+					pcontrol(3) <= pcontrol(3);
 					pcontrol(4) <= datain(0);
-					pcontrol(5) <- datain(1);
+					pcontrol(5) <= datain(1);
 					pcontrol(6) <= datain(2);
 					pcontrol(7) <= datain(3);
 					edo_futuro <= d0;
 				WHEN d15 =>
 					cs <= "10010";
-					Ew - '0';
+					rw - '0';
 					edo_futuro <= d0;
 				WHEN d16 =>
-					cs <- "10000";
+					cs <= "10000";
 					f(0) <= datain(0);
 					f(1) <= datain(1);
 					f(2) <= datain(2);
 					f(3) <= '0';
-					£(4) <- '0';
+					£(4) <= '0';
 					f(5) <= '0';
 					f(6) <= '0';
-					£(7) <- '0';
+					£(7) <= '0';
 					IF datain(3) = '0' THEN
-						pcontrok <- pcout + f;
+						pcontrol <= pcout + f;
 						edo_futuro <= d17;
 					ELSE
-						pcontrol <- pcout - f;
+						pcontrol <= pcout - f;
 						edo_futuro <= d17;
 					END IF;
-				WHEN d17 ->
-					cs <- "11110";
-					edo_ futuro <- d0;
+				WHEN d17 =>
+					cs <= "11110";
+					edo_ futuro <= d0;
 			END CASE;
 		END IF;
 	END PROCESS procesoA;
@@ -204,10 +206,10 @@ BEGIN
 	BEGIN
 		IF (clk' event AND clk = '1') THEN
 			IF reset = '0' THEN
-				edo_presente <= edo_ futuro;
+				edo_presente <= edo_futuro;
 			ELSE
 				NULL;
 			END IF;
 		END IF;
 	END PROCESS procesoB;
-END Pshaviorali
+END Behavioral;
