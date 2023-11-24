6d26eee329822d77944f4b6f42e576724663335d
Alan <113253976+MrDonkey08@users.noreply.github.com>
2023-11-24 14:24:39 -0600

    Rankine

--- programacion-para-internet/actividades/pagina-web-personal/html/calculadoras/js/temperaturas.js
+++ programacion-para-internet/actividades/pagina-web-personal/html/calculadoras/js/temperaturas.js
@@ -28,24 +28,40 @@ function getTemp() {
 			case "C":
 				if (optTempOut === "F") {
 					temp = (inputTemp * 9) / 5 + 32;
-				} else {
+				} else if (optTempOut === "K") {
 					temp = inputTemp + 273.15; // C->K
+				} else {
+					temp = (inputTemp * 9) / 5 + 491.67;
 				}
 				break;
 
 			case "F":
 				if (optTempOut === "C") {
 					temp = ((inputTemp - 32) * 5) / 9;
-				} else {
+				} else if (optTempOut === "K") {
 					temp = (inputTemp * 9) / 5 + 273.5; // F->K
+				} else {
+					temp = inputTemp + 459.67;
 				}
 				break;
 
 			case "K":
 				if (optTempOut === "C") {
 					temp = inputTemp - 273.15;
-				} else {
+				} else if (optTempOut === "F") {
 					temp = ((inputTemp - 273.15) * 9) / 5 + 32; // K->F
+				} else {
+					temp = (inputTemp * 9) / 5;
+				}
+				break;
+
+			case "Ra":
+				if (optTempOut === "C") {
+					temp = (inputTemp - 491.67) * (5 / 9);
+				} else if (optTempOut === "F") {
+					temp = inputTemp - 459.67;
+				} else {
+					temp = (inputTemp * 5) / 9;
 				}
 				break;
 		}
