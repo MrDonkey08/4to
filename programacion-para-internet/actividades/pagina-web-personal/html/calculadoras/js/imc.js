ef7030d53a749bfd26773e66f77eb23949cddf09
Alan <113253976+MrDonkey08@users.noreply.github.com>
2023-11-24 12:52:20 -0600

    Colores IMC

--- programacion-para-internet/actividades/pagina-web-personal/html/calculadoras/js/imc.js
+++ programacion-para-internet/actividades/pagina-web-personal/html/calculadoras/js/imc.js
@@ -11,6 +11,7 @@ const inputComp = document.getElementById("input-composicion-corporal");
 
 let IMC;
 let comp;
+let estado;
 
 function getIMC() {
 	const altura = inputAltura.value / 100;
@@ -27,30 +28,66 @@ function getIMC() {
 	*/
 
 	inputIMC.value = IMC;
-
 	inputComp.value = getComp();
+	colorEstado();
 }
 
 function getComp() {
+	/* Status
+		- 4 = severo -> rojo
+		- 3 = alto -> naranja
+		- 2 = medio -> amarillo
+		- 1 = normal - > verde
+	 */
+
 	if (IMC >= 40) {
+		estado = 4;
 		comp = "Obesidad tipo 3 (> 40)";
 	} else if (IMC >= 35) {
+		estado = 4;
 		comp = "Obesidad tipo 2 (35 - 40)";
 	} else if (IMC >= 30) {
+		estado = 3;
 		comp = "Obesidad tipo 1 (30 - 34)";
 	} else if (IMC >= 25) {
+		estado = 2;
 		comp = "Sobrepeso (25 - 29)";
 	} else if (IMC >= 18.5) {
+		estado = 1;
 		comp = "Normal (18.5 - 24)";
 	} else if (IMC >= 17) {
+		estado = 2;
 		comp = "Delgadez leve (17 - 18.4)";
 	} else if (IMC >= 16) {
+		estado = 3;
 		comp = "Delgadez media (16 - 16.9)";
 	} else {
+		estado = 4;
 		comp = "Delgadez severa (> 16)";
 	}
 
 	return comp;
+}
+
+function colorEstado() {
+	switch (estado) {
+		case 1: // verde
+			inputIMC.style.color = inputComp.style.color = "#006100";
+			inputIMC.style.backgroundColor = inputComp.style.backgroundColor = "#C6EDCE";
+			break;
+		case 2: // amarillo
+			inputIMC.style.color = inputComp.style.color = "#E7B10A";
+			inputIMC.style.backgroundColor = inputComp.style.backgroundColor = "#FFFEC4";
+			break;
+		case 3: // naranja
+			inputIMC.style.color = inputComp.style.color = "#F94C10";
+			inputIMC.style.backgroundColor = inputComp.style.backgroundColor = "#EFD595";
+			break;
+		case 4: // rojo
+			inputIMC.style.color = inputComp.style.color = "#9C0006";
+			inputIMC.style.backgroundColor = inputComp.style.backgroundColor = "#FFC7CE";
+			break;
+	}
 }
 
 btnIMC.addEventListener("click", getIMC);
